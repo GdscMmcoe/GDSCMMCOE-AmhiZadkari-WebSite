69c77d0098381901632e03a411f7389183132d99
asyncdoggo <ayushdeshpande81@gmail.com>
2023-03-12 18:53:46 +0530

    Add zadkari logo in navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,16 @@
 import { useEffect, useState, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../assets/images/icons/banner1-crop.png"
 import Zadkari_1 from "../assets/images/icons/Zadkari_1.png";
 import Zadkari_2 from "../assets/images/icons/Zadkari_2.png";
 
 
 export default function NavBar() {
     const [navBg, setNavBg] = useState("");
-    const [navLogo, setNavLogo] = useState("");
     const menuRef = useRef()
     const [text, setText] = useState("text-white");
     const [lineColor, setLineColor] = useState("md:from-white md:to-white from-black to-black");
     const location = useLocation();
+    const [logo, setLogo] = useState(Zadkari_1);
 
 
     const listenScrollEvent = () => {
@@ -19,13 +18,13 @@ export default function NavBar() {
         if (window.scrollY <= 10) {
             setNavBg("bg-white md:bg-transparent")
             setText("text-black md:text-white")
-            // setNavLogo(<img src={Zadkari_2}/>)
+            setLogo(Zadkari_2)
 
         } else if (window.scrollY > 10) {
             setNavBg("bg-white")
             setText("text-black")
             setLineColor("from-black to-black")
-            // setNavLogo(<img src={Zadkari_1}/>)
+            setLogo(Zadkari_1)
         }
     };
 
@@ -42,12 +41,14 @@ export default function NavBar() {
         setText("text-black md:text-white")
         setLineColor("from-white to-white")
         if (location.pathname === "/") {
+            setLogo(Zadkari_2)
             window.addEventListener("scroll", listenScrollEvent);
             window.addEventListener("resize", menuChecked)
             return () => {
                 window.removeEventListener("scroll", listenScrollEvent);
             };
         } else {
+            setLogo(Zadkari_1)
             setNavBg("bg-white")
             setText("text-black")
             setLineColor("from-black to-black")
@@ -96,25 +97,18 @@ export default function NavBar() {
                     />
                     <div className="w-full flex flex-row place-content-between items-center md:hidden">
                         <div className="w-64 px-3 hover:cursor-pointer">
-                            <Link to="/"> <img src={Zadkari_1} alt="logo" className="" /></Link>
+                            <Link to="/"> <img src={Zadkari_1} alt="logo" className="w-24" /></Link>
                         </div>
-                        {/* <label className={`px-5 text-2xl text-black font-bold`}>
-                            Amhi Zadkari
-                        </label> */}
+
                         <label htmlFor="menu" className="flex flex-col py-2 group" >
                             <span className="border-t-2 border-black w-9 h-3 mt-3 mr-2"></span>
                             <span className="border-t-2 border-black w-9 h-3 mr-2"></span>
                             <span className="border-t-2 border-black w-9 h-3 mr-2"></span>
                         </label>
                     </div>
-                    <Link to="/" className="w-20 px-3 py-4 hover:cursor-pointer hidden md:block">
-                        {/* <img src={Zadkari_1} alt="logo" className="" /> */}
-                        <img src={Zadkari_2} alt="logo" className="" />
+                    <Link to="/" className="w-24 px-3 py-4 hover:cursor-pointer hidden md:block">
+                        <img src={logo} alt="logo" className="w-24" />
                     </Link>
-
-                    {/* <div className={`w-64 py-5 px-5 hidden text-2xl font-bold md:block ` + text}>
-                        <p>Amhi Zadkari</p>
-                    </div> */}
                     <nav
                         className={`navitems hidden w-full justify-end lg:pr-32 lg:justify-center md:flex peer-checked:block peer-checked:bg-white md:py-2 `}
                     >
